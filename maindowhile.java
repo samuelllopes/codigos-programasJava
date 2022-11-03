46293d1b7765a2626e8b0fbb37e43243d714fe51
Samuel Lopes <103187575+samuelllopes@users.noreply.github.com>
2022-11-03 20:05:53 -0300

    Update maindowhile.java

--- maindowhile.java
+++ maindowhile.java
@@ -2,82 +2,81 @@
 
 1. Faça um programa que solicite ao usuário que informe a matrícula e as três notas de um conjunto de alunos. O programa deverá exibir a mensagem informando se o aluno foi aprovado (média maior ou igual a 70), exame (nota maior ou igual a 60 e menor que 70) ou reprovado (nota inferior a 60). O programa irá terminar quando o usuário informar uma matrícula negativa. 
 
-                import java.util.Scanner;
-                import java.lang.Math;
-                public class Main{
+import java.util.Scanner;
+import java.lang.Math;
+public class Main{
 
-            public static void main (String[]args){
+ public static void main (String[]args){
 
-          Scanner leia = new Scanner (System.in);
+    Scanner leia = new Scanner (System.in);
 
-          int  i, validar, validarmatricula, j, k;
-          int aluno[] = new int[4];
-          double matricula[] = new double[4];
-          double nota[] = new double[3];
-          double media;
+ int  i, validar, validarmatricula, j, k;
+ int aluno[] = new int[4];
+ double matricula[] = new double[4];
+ double nota[] = new double[3];
+ double media;
 
-          System.out.println ("-----INFORME A MATRICULA----- ");
+ System.out.println ("-----INFORME A MATRICULA----- ");
 
-          for (i = 0; i < 4; i++) {
-          System.out.print ("Digite a matricula do aluno " + (i+1) + ": ");
-          matricula[i] = leia.nextDouble ();
-          aluno[i] = i;
-          }
+ for (i = 0; i < 4; i++) {
+   System.out.print ("Digite a matricula do aluno " + (i+1) + ": ");
+    matricula[i] = leia.nextDouble ();
+    aluno[i] = i;
+}
 
-          System.out.println ("-----SISTEMA DE NOTAS----- ");
-          do{
-          validar = 0;
+   System.out.println ("-----SISTEMA DE NOTAS----- ");
+    do{ 
+    validar = 0;
 
-          System.out.print ("Digite a matricula do aluno: ");
-          validarmatricula = leia.nextInt ();
-          for (i = 0; i < 4; i++){
-          if (validarmatricula == matricula[i]){
+  System.out.print ("Digite a matricula do aluno: ");
+    validarmatricula = leia.nextInt ();
+    for (i = 0; i < 4; i++){
+    if (validarmatricula == matricula[i]){
 
-          media = 0;
+    media = 0;
 
-          for (j = 0; j < 3; j++) {
+   for (j = 0; j < 3; j++) {
 
-           System.out.print ("Digite a " + (j + 1) + "ª nota do aluno " + (i+1) + ": ");
-              nota[j] = leia.nextDouble();
-              media = media + nota[j];
-          }
-              media = media/3;
+   System.out.print ("Digite a " + (j + 1) + "ª nota do aluno " + (i+1) + ": ");
+    nota[j] = leia.nextDouble();
+    media = media + nota[j];
+ }
+    media = media/3;
 
-              System.out.println ("As notas do aluno " + (i+1) + " foram cadastradas com sucesso!");
-              System.out.println ("-----------RESULTADO-----------");
-              System.out.println (" A media do aluno " + (i+1) + " foi: " + media);
+  System.out.println ("As notas do aluno " + (i+1) + " foram cadastradas com sucesso!");
+  System.out.println ("-----------RESULTADO-----------");
+  System.out.println (" A media do aluno " + (i+1) + " foi: " + media);
 
+if(media >= 70){
+   System.out.println (" O aluno " + (i+1) + " foi APROVADO");
+}   
+   else if(media > 60){
+     System.out.println (" O aluno " + (i+1) + " deverá fazer EXAME DE RECUPERAÇÃO");
+}
+   else{
+     System.out.println(" O aluno " + (i+1) + " foi REPROVADO");
+ }
+   i = 4;
 
-              if(media >= 70){
-                  System.out.println (" O aluno " + (i+1) + " foi APROVADO");
-          }   
-              else if(media > 60){
-                  System.out.println (" O aluno " + (i+1) + " deverá fazer EXAME DE RECUPERAÇÃO");
-          }
-              else{
-                  System.out.println(" O aluno " + (i+1) + " foi REPROVADO");
-          }
-               i = 4;
+  System.out.println("------------------------");
+  System.out.println("NOVO LANÇAMENTO");
+  System.out.println("------------------------");
 
-               System.out.println("------------------------");
-               System.out.println("NOVO LANÇAMENTO");
-               System.out.println("------------------------");
+ validar = 0;
+ } 
+   else{
 
-              validar = 0;
-              } 
-                 else{
+  validar = 1;
+  }
+     }
+       }
 
-               validar = 1;
-           }
-                }
-                  }
+  while(validar == 0);
+  System.out.println(" MATRICULA INVÁLIDA. PROGRAMA FINALIZADO");
 
-           while(validar == 0);
-           System.out.println(" MATRICULA INVÁLIDA. PROGRAMA FINALIZADO");
+  }
 
-           }
-
-            }
+ }
 
 2. Leia a idade de um número indeterminado de pessoas. Imprima:
 - Quantas pessoas possuem idade acima de 50 anos
