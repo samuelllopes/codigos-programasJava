f3c141356101e066286865b5c91962c6ab108bb0
Samuel Lopes <103187575+samuelllopes@users.noreply.github.com>
2022-11-04 19:29:20 -0300

    Update mainfor.java

--- mainfor.java
+++ mainfor.java
@@ -1,4 +1,4 @@
-## Programação de Soluções Computacionais / Estruturas de Repetição - FOR
+Programação de Soluções Computacionais / Estruturas de Repetição - FOR
 
 
 1 - Faça um programa que faça a leitura de 5 valores, e para cada valor, mostre o seu dobro na tela. 
