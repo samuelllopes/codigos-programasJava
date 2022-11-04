0c04acb436cf7179467b1cf60a7e31fc46fa864a
Samuel Lopes <103187575+samuelllopes@users.noreply.github.com>
2022-11-04 19:29:39 -0300

    Update maindowhile.java

--- maindowhile.java
+++ maindowhile.java
@@ -1,4 +1,4 @@
-## Programação de Soluções Computacionais - Estruturas de Repetição – WHILE e DO-WHILE
+Programação de Soluções Computacionais - Estruturas de Repetição – WHILE e DO-WHILE
 
 1. Faça um programa que solicite ao usuário que informe a matrícula e as três notas de um conjunto de alunos. O programa deverá exibir a mensagem informando se o aluno foi aprovado (média maior ou igual a 70), exame (nota maior ou igual a 60 e menor que 70) ou reprovado (nota inferior a 60). O programa irá terminar quando o usuário informar uma matrícula negativa. 
 
