e2924e3fd16136865e0834f25289d3ed3f44c348
Samuel Lopes <103187575+samuelllopes@users.noreply.github.com>
2022-11-04 19:30:09 -0300

    Update maincondicional.java

--- maincondicional.java
+++ maincondicional.java
@@ -1,4 +1,4 @@
-## Programação de Soluções Computacionais - Estrutura Condicional
+Programação de Soluções Computacionais - Estrutura Condicional
 
 
 1. Faça um programa que receba dois números e mostre o maior e o menor. Emita uma mensagem, caso os dois sejam iguais.
