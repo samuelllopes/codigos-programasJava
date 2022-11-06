f65dd631b2f0f965b4ebdddb67bbdff3748359f6
Samuel Lopes <103187575+samuelllopes@users.noreply.github.com>
2022-11-06 17:34:16 -0300

    Update maincondicional.java

--- maincondicional.java
+++ maincondicional.java
@@ -269,12 +269,103 @@ public class Main{
 
 10. Faça um programa que leia o um número inteiro entre 1 e 7 e escreva o dia da semana correspondente. Caso o usuário digite um número fora desse intervalo, deverá aparecer uma mensagem informando que não existe dia da semana com esse número.
 
+import java.util.Scanner;
+public class Main{
 
+    public static void main(String[] args) {
+        
+    int numdia;
+        
+    Scanner teclado = new Scanner(System.in);
+        
+    System.out.println("Digite um número de 1 a 7: ");
+    numdia = teclado.nextInt(); 
+        
+    
+    if(numdia < 0 || numdia > 8 ){
+	  System.out.println("Não existe dia da semana");
+    } 
+	else if(numdia == 1) {
+	  System.out.println("Domingo");
+	}
+    else if(numdia == 2) {
+	  System.out.println("Segunda");
+	}
+    else if(numdia == 3) {
+	  System.out.println("Terça");
+	}
+    else if(numdia == 4) {
+	  System.out.println("Quarta");
+	}
+	else if(numdia == 5) {
+	  System.out.println("Quinta");
+	}
+	else if(numdia == 6) {
+	  System.out.println("Sexta");
+	}
+	else if(numdia == 7) {
+	  System.out.println("Sábado");
+	}
+}
+}
 
 
 12. Faça um programa que leia um número inteiro entre 1 e 12 e escrever o mês correspondente. Caso o usuário digite um número fora desse intervalo, deverá aparecer uma mensagem informando que não existe mês com este número.
 
+import java.util.Scanner;
+public class Main{
 
+    public static void main(String[] args) {
+        
+    int nummes;
+        
+    Scanner teclado = new Scanner(System.in);
+        
+    System.out.println("Digite um número de 1 a 12: ");
+    nummes = teclado.nextInt(); 
+        
+    
+    if(nummes < 0 || nummes > 12 ){
+	  System.out.println("Não existe dia da semana");
+    } 
+	else if(nummes == 1) {
+	  System.out.println("Janeiro");
+	}
+    else if(nummes == 2) {
+	  System.out.println("Fevereiro");
+	}
+    else if(nummes == 3) {
+	  System.out.println("Março");
+	}
+    else if(nummes == 4) {
+	  System.out.println("Abril");
+	}
+	else if(nummes == 5) {
+	  System.out.println("Maio");
+	}
+	else if(nummes == 6) {
+	  System.out.println("Junho");
+	}
+	else if(nummes == 7) {
+	  System.out.println("Julho");
+	}
+	else if(nummes == 8) {
+	  System.out.println("Agosto");
+	}
+	else if(nummes == 9) {
+	  System.out.println("Setembro");
+	}
+	else if(nummes == 10) {
+	  System.out.println("Outubro");
+	}
+	else if(nummes == 11) {
+	  System.out.println("Novembro");
+	}
+	else if(nummes == 11) {
+	  System.out.println("Dezembro");
+	}
+}
+}
 
 
 14. Faça um programa que solicite ao usuário que informe dois números e que exiba o seguinte menu:
